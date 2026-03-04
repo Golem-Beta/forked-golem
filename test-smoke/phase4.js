32cec21cb949e85c9b068beda70ec9023ac2de3f
golem-beta <golembeta01@gmail.com>
2026-03-04 21:58:21 +0800

    test: 補充 model-benchmark 子模組 smoke tests

--- test-smoke/phase4.js
+++ test-smoke/phase4.js
@@ -156,5 +156,31 @@ module.exports = function phase4(test) {
         });
     }
 
+    // model-benchmark 子模組
+    test('model-benchmark/index exports ModelBenchmarkAction class', () => {
+        const ModelBenchmarkAction = require('../src/autonomy/actions/model-benchmark/index');
+        assert(typeof ModelBenchmarkAction === 'function');
+        assert(typeof ModelBenchmarkAction.prototype.run === 'function');
+    });
+    test('model-benchmark/runner exports BenchmarkRunner class', () => {
+        const BenchmarkRunner = require('../src/autonomy/actions/model-benchmark/runner');
+        assert(typeof BenchmarkRunner === 'function');
+        assert(typeof BenchmarkRunner.prototype.run === 'function');
+    });
+    test('model-benchmark/targets exports buildTargets function', () => {
+        const { buildTargets } = require('../src/autonomy/actions/model-benchmark/targets');
+        assert(typeof buildTargets === 'function');
+    });
+    test('model-benchmark/tests exports TESTS array and MAX_SCORE', () => {
+        const { TESTS, MAX_SCORE } = require('../src/autonomy/actions/model-benchmark/tests');
+        assert(Array.isArray(TESTS) && TESTS.length === 4);
+        assert(typeof MAX_SCORE === 'number');
+    });
+    test('model-benchmark/callers exports callGemini and callOpenAICompat', () => {
+        const { callGemini, callOpenAICompat } = require('../src/autonomy/actions/model-benchmark/callers');
+        assert(typeof callGemini === 'function');
+        assert(typeof callOpenAICompat === 'function');
+    });
+
     return s;
 };
